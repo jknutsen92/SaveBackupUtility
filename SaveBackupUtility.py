38e67152c5bc4fb4cb23fa15b8d0862b9e04358d
Jeffrey Knutsen <jknutsen92@gmail.com>
2025-12-17 21:16:04 -0700

    Refactored logic to be simpler

--- SaveBackupUtility.py
+++ SaveBackupUtility.py
@@ -1,4 +1,4 @@
-from sys import argv
+from sys import argv, exit
 from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -6,20 +6,7 @@ from shutil import copyfile
 from uuid import uuid4
 import time
 
-class SaveChangedHandler(FileSystemEventHandler):
-    def __init__(self, backup_dir):
-        self._backup_dir = backup_dir
-
-    def on_modified(self, event):
-        if not event.src_path == "":
-            save_path = Path(event.src_path)
-            unique_name = f"{save_path.name}_{str(uuid4())}"
-            copyfile(save_path, f"{self._backup_dir}/{unique_name}")
-            print(f"{save_path.name} updated, making backup {unique_name} in {self._backup_dir}")
-        
-
 if __name__ == "__main__":
-
     if len(argv) < 2:
         raise RuntimeError("Not enough arguments, please provide the path to a save file")
 
@@ -37,15 +24,15 @@ if __name__ == "__main__":
         print(f"Backup directory {backup_dir} exists")
     print(f"backing up saves to {backup_dir}")
 
-    event_handler = SaveChangedHandler(backup_dir)
-    observer = Observer()
-    observer.schedule(event_handler, path=save_path, recursive=False)
-    observer.start()
     print(f"Now watching {save_path} for updates")
 
     try:
         while True:
+            prev_modified_time = save_path.stat().st_mtime
             time.sleep(5)
+            if prev_modified_time < save_path.stat().st_mtime:
+                unique_name = f"{save_path.name}_{str(uuid4())}"
+                copyfile(save_path, f"{backup_dir}/{unique_name}")
+                print(f"{save_path.name} updated, making backup {unique_name} in {backup_dir}")
     except KeyboardInterrupt:
-            observer.stop()
-    observer.join()
+        exit()
